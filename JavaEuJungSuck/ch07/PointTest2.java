815cb322df7d29c1c07464fdb4001faa6099476f
Link <88365662+GimLink@users.noreply.github.com>
2023-06-04 00:00:23 +0900

    Add files via upload

---
+++ JavaEuJungSuck/ch07/PointTest2.java
@@ -0,0 +1,26 @@
+package ch07;
+
+public class PointTest2 {
+
+	public static void main(String[] args) {
+
+		Point3D2 p3 = new Point3D2();
+		System.out.println("p3.x ="+p3.x);
+		System.out.println("p3.y ="+p3.y);
+		System.out.println("p3.z ="+p3.z);
+	}
+
+}
+
+class Point3D2 extends Point1{
+	int z = 30;
+	
+	Point3D2(){
+		this(100, 200, 300);
+	}
+	
+	Point3D2(int x, int y, int z){
+		super(x, y);
+		this.z = z;
+	}
+}
