834e1ee1124e332b9a5355f6697d5597e1120850
Link <88365662+GimLink@users.noreply.github.com>
2023-07-06 23:20:23 +0900

    Add files via upload

---
+++ JavaEuJungSuck/ch11/AsciiPrint.java
@@ -0,0 +1,15 @@
+package ch11;
+
+import java.util.*;
+
+public class AsciiPrint {
+
+	public static void main(String[] args) {
+		char ch = ' ';
+		for(int i=0; i <95; i++) {
+			System.out.print(ch++);
+		}
+	}
+	
+
+}
